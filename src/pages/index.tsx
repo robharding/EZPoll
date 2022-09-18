94a82c5cfdabaad2eeffe2a145f7c3f4b89c8533
Rob Harding <robharding@Robs-MacBook-Air.local>
2022-09-18 11:23:41 -0600

    Setup prisma & planetscale + basic testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,32 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-const Home: NextPage = () => {
+import { prisma } from "../db/client";
+
+const Home: NextPage = (props: any) => {
     return (
         <>
             <Head>
-                <title>Hello, World!</title>
+                <title>EZPoll</title>
+                <meta name="description" content="A simple polling app" />
             </Head>
 
-            <h1 className="text-3xl font-bold underline">Hello, World!</h1>
+            <main>
+                <h1 className="text-3xl font-bold underline">Hello, World!</h1>
+                <code>{props.questions}</code>
+            </main>
         </>
     );
+};
+
+export const getServerSideProps = async () => {
+    const questions = await prisma.pollQuestion.findMany();
+
+    return {
+        props: {
+            questions: JSON.stringify(questions),
+        },
+    };
 };
 
 export default Home;
